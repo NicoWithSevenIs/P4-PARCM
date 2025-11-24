6ad0acf28cf993ad61878ef37c06b2ca50afcad9
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2025-11-25 05:54:54 +0800

    init

---
+++ P4-PARCM.cpp
@@ -0,0 +1,12 @@
+﻿// P4-PARCM.cpp : Defines the entry point for the application.
+//
+
+#include "P4-PARCM.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
