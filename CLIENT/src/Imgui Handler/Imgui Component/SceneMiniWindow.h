63ab3a867039ed46438f755802dea0bdb08f48f6
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2025-12-05 01:54:39 +0800

    ok

--- CLIENT/src/Imgui Handler/Imgui Component/SceneMiniWindow.h
+++ CLIENT/src/Imgui Handler/Imgui Component/SceneMiniWindow.h
@@ -26,7 +26,8 @@ class SceneMiniWindow final : public ImguiComponent {
 
 		if (ImGui::ImageButton(tex,ImVec2(64, 64), ImVec2(0, 0),ImVec2(1, 1), 0, ImVec4(0, 0, 0, 0), ImVec4(1, 1, 1, 1)))
 		{
-			printf("You clicked the image button!\n");
+			if(NetworkHandler::get().progress[index] == 1)
+				GameObjectManager::LoadScene(NetworkHandler::get().scene_cache[index]);
 		}
 
 		ImGui::ProgressBar(NetworkHandler::get().progress[index]);
